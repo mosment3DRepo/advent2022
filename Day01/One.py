740665bc3fb13abcf0e380597b21288924bb6a7c
mosment3DRepo <matthew.osment@3drepo.org>
2022-12-03 19:41:56 +0000

    updated to simplify

--- Day01/One.py
+++ Day01/One.py
@@ -1,16 +1,15 @@
 elves = open("Day01\\input.txt", "r").read().split("\n\n")
-bigCal = [0,0,0]
+bigCal = []
 currentCal = 0
 for elf in elves:
     fruitList = elf.split("\n")
     for fruit in fruitList:
-        currentCal = currentCal + float(fruit or 0)
-        if currentCal > bigCal[0]:
-            bigCal[0] = currentCal
-            bigCal.sort()
+        currentCal += float(fruit or 0)
+    bigCal.append(currentCal)
     currentCal = 0
-print(bigCal[2])
-print(sum(bigCal))
+bigCal.sort(reverse=True)
+print(bigCal[0])
+print(sum(bigCal[:2]))
         
 
 
