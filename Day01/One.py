251b30aec5c8dd8fca428afd310e59f23cbd9398
mosment3DRepo <matthew.osment@3drepo.org>
2022-12-03 14:36:22 +0000

    complete answer

--- Day01/One.py
+++ Day01/One.py
@@ -1,23 +1,16 @@
-# For each elf total the calories
-# if it's higher than the current variable then add that one
-
-f = open("Day01\\input.txt", "r")
-input = f.read()
-
-elves = input.split("\n\n")
-
-bigCal = 0
+elves = open("Day01\\input.txt", "r").read().split("\n\n")
+bigCal = [0,0,0]
 currentCal = 0
-
 for elf in elves:
     fruitList = elf.split("\n")
     for fruit in fruitList:
         currentCal = currentCal + float(fruit or 0)
-        if currentCal > bigCal:
-            bigCal = currentCal
+        if currentCal > bigCal[0]:
+            bigCal[0] = currentCal
+            bigCal.sort()
     currentCal = 0
-
-print(bigCal)
+print(bigCal[2])
+print(sum(bigCal))
         
 
 
