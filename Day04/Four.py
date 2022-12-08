564ae675dd80a710464029a9ae6708c2957de500
mosment3DRepo <matthew.osment@3drepo.org>
2022-12-08 23:06:17 +0000

    day for part 2

--- Day04/Four.py
+++ Day04/Four.py
@@ -1,8 +1,16 @@
 count = 0
+count2 = 0
 with open("Day04\\input.txt", 'r') as f:
     for pairs in f.readlines():
         sPairs = pairs.replace('-',',')
-        sPairs = sPairs.strip().split(',')        
-        if (int(sPairs[3])-int(sPairs[1]))*(int(sPairs[2])-int(sPairs[0]))<=0:
-            count+= 1
+        sPairs = sPairs.strip().split(',')         
+        sPairsInt = list(map(lambda a: int(a), sPairs))
+        smaller = sPairsInt.index(min(sPairsInt))   
+        if (sPairsInt[3]-sPairsInt[1])*(sPairsInt[2]-sPairsInt[0])<=0:
+            count += 1
+        if sPairsInt[smaller+1] >= sPairsInt[abs(smaller-2)]:
+            count2 += 1
+
+        
 print(count)
+print(count2)
