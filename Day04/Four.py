b24f697a0e7725badf708ddc209fa461058c437b
mosment3DRepo <matthew.osment@3drepo.org>
2022-12-08 23:23:20 +0000

    cleaned logic

--- Day04/Four.py
+++ Day04/Four.py
@@ -1,14 +1,17 @@
 count = 0
 count2 = 0
+
+
 with open("Day04\\input.txt", 'r') as f:
     for pairs in f.readlines():
         sPairs = pairs.replace('-',',')
         sPairs = sPairs.strip().split(',')         
-        sPairsInt = list(map(lambda a: int(a), sPairs))
-        smaller = sPairsInt.index(min(sPairsInt))   
+        sPairsInt = list(map(int, sPairs))
+        f = sPairsInt.index(min(sPairsInt))   
         if (sPairsInt[3]-sPairsInt[1])*(sPairsInt[2]-sPairsInt[0])<=0:
-            count += 1
-        if sPairsInt[smaller+1] >= sPairsInt[abs(smaller-2)]:
+            if sPairsInt[f+1] >= sPairsInt[abs(f-3)] or sPairsInt[f] == sPairsInt[abs(f-2)]:
+                count += 1
+        if sPairsInt[f+1] >= sPairsInt[abs(f-2)]:
             count2 += 1
 
         
