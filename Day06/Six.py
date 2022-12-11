f2b89c2a0d334e55b8959239ba02623d985e6905
mosment3DRepo <matthew.osment@3drepo.org>
2022-12-11 01:25:20 +0000

    day 6 efficient version

--- Day06/Six.py
+++ Day06/Six.py
@@ -1,21 +1,14 @@
 lastFour = []
-listCount=0
 
 with open("Day06\\input.txt", 'r') as f:
     signal = f.read()
-    for idx, x in enumerate(signal):
-        if x not in lastFour:            
-            if listCount == 3 and len(lastFour) ==3:
-                print(idx + 1)
-                exit
-            else:
-                listCount += 1
-                lastFour.append(x)     
-        else:
-            if listCount != 3:
-                listCount += 1
-            else:
-                del lastFour[0]
-        print(x)
+    for ind, x in enumerate(signal):
+        try:
+            del lastFour[:lastFour.index(x)+1]
+        except:
+            if len(lastFour) == 3:
+                print('done! '+ str(ind+1)+ ' ' + str(lastFour + [x]))
+                break
+        lastFour.append(x) 
 
     
