7fc3a4193e6f9ab85159ad0642e49c048775dc59
mosment3DRepo <62879582+mosment3DRepo@users.noreply.github.com>
2022-12-23 11:22:42 +0000

    streamlined Day 7

--- Day07/Seven.py
+++ Day07/Seven.py
@@ -1,39 +1,35 @@
 SPACE_TARGET = 30000000
 
-list = False
 activeDir=[]
 fileArray = []
 dirTotals = {}
 partOne = 0
 spaceAvail = 70000000
-lowestDelete = 70000000
+
 
 with open("Day07\\input.txt", 'r') as f:
     commands = f.read().splitlines()
     for c in commands:
-        if c[:4] == '$ ls':
-            list = True
+        if c[:3] in ('$ l','dir'):
             continue
-        if c == '$ cd ..':
-            activeDir.pop()
-            list = False
-            continue
-        if c[:4] == '$ cd':
-            activeDir.append(c[5:])
-            list = False
-            continue
-        if list == True and c[:3] != 'dir':
-            size = int(c.split(' ')[0])
-            spaceAvail -= size
-            for i in range(len(activeDir)):
-                loc = activeDir[:i+1]
-                locString = '/'.join(loc)
-                try:
-                    dirTotals[locString] = dirTotals[locString] + size
-                except KeyError:
-                    dirTotals[locString] = size
+        if c[:1] == '$':
+            if c == '$ cd ..':
+                activeDir.pop()
+            else:
+                activeDir.append(c[5:])
+            continue        
+        size = int(c.split(' ')[0])
+        spaceAvail -= size
+        for i in range(len(activeDir)):
+            loc = activeDir[:i+1]
+            locString = '/'.join(loc)
+            try:
+                dirTotals[locString] = dirTotals[locString] + size
+            except KeyError:
+                dirTotals[locString] = size
 
 spaceNeeded = SPACE_TARGET - spaceAvail
+lowestDelete = 70000000
 
 for item in dirTotals.items():
     if spaceNeeded < item[1] < lowestDelete:
